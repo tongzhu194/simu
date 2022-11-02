d71f3ec8500742250fe8786854d87f2579eac27c
Tong Zhu <tzhu@holygpu7c1311.rc.fas.harvard.edu>
2022-11-02 16:05:01 -0400

    debug

--- pipev3_metrics.py
+++ pipev3_metrics.py
@@ -22,18 +22,23 @@ print(f'GPUs available: {tf.config.list_physical_devices("GPU")}')
 
 import knndatap
 
-
+#Last edit: 1027 we have id in context now
+#Last edit: 1102 we have ortho filter for interstring things now
 ##########################
 
 graph_schema = tfgnn.read_schema("/n/home05/tzhu/work/icgen2/algorithm/schema_v3_metrics.txt")
 gtspec = tfgnn.create_graph_spec_from_schema_pb(graph_schema)
 
+interfilter = True
+index = 2
+efffile = f'/n/home05/tzhu/work/icgen2/simu/ortho_eff_{index}.npy'
 
 ########################
 def makeGT(id,counter):
     print("Generating GraphTensor for this event: ID =",id)
     hit_num = len(simu.loc[id][2]['sensor_pos_x'])+len(simu.loc[id][3]['sensor_pos_x'])
-    hits = np.array([np.append(simu.loc[id][2]['t'],simu.loc[id][3]['t']),np.append(simu.loc[id][2]['sensor_pos_x'],simu.loc[id][3]['sensor_pos_x']),np.append(simu.loc[id][2]['sensor_pos_y'],simu.loc[id][3]['sensor_pos_y']),np.append(simu.loc[id][2]['sensor_pos_z'],simu.loc[id][3]['sensor_pos_z'])]).transpose()
+    hits = np.array([np.append(simu.loc[id][2]['t'],simu.loc[id][3]['t']),np.append(simu.loc[id][2]['sensor_pos_x'],simu.loc[id][3]['sensor_pos_x']),np.append(simu.loc[id][2]['sensor_pos_y'],simu.loc[id][3]['sensor_pos_y']),np.append(simu.loc[id][2]['sensor_pos_z'],simu.loc[id][3]['sensor_pos_z']),np.append(simu.loc[id][2]['string_id'],simu.loc[id][3]['string_id'])]).transpose()
+ 
     hits = hits[hits[:,0].argsort()] 
     ind = []
     for i in range(hit_num):
@@ -41,15 +46,24 @@ def makeGT(id,counter):
         else:break
     hits = np.delete(hits,ind,axis=0)
     hit_num = hit_num-len(ind)
+    #print(f'####:{hit_num}')
+    if interfilter == True:
+        eff = np.load(efffile)
+        map = np.isin(hits[:,4],eff)
+        hits = hits[map]
+        hits = hits[:,0:3]
+        hit_num= len(hits)
+
     print("There is",hit_num,"hit(s) in this event.")
     #print(hits)
     fenergy = simu.loc[id][1]['injection_energy']
     fzenith = simu.loc[id][1]['injection_zenith']
     fazimuth = simu.loc[id][1]['injection_azimuth']
     feventid = tf.cast(counter,dtype=tf.int64)
-    
+    fhitnum = tf.cast(hit_num,dtype = tf.int64) 
     hits_input = hits.flatten()
-
+    if(hit_num>1300000):
+        return 0
     stbinding = knndatap.knndatap(hits_input,hit_num)
 
     
@@ -83,7 +97,7 @@ def makeGT(id,counter):
         adjacency=hit_adjacency)
 
     context = tfgnn.Context.from_fields(
-        features={"injection_zenith":[fzenith],"event_energy":[fenergy],"injection_azimuth":[fazimuth],"event_id":[feventid]})
+        features={"injection_zenith":[fzenith],"event_energy":[fenergy],"injection_azimuth":[fazimuth],"event_id":[feventid],"hit_num":[fhitnum]})
 
     graphtensor = tfgnn.GraphTensor.from_pieces(node_sets={"hit":hit},edge_sets={"coincidence":coincidence},context=context)
     #print("%%%%%%%%%%%")
@@ -93,9 +107,12 @@ def makeGT(id,counter):
 
 ##############################
 counter =0
-with tf.io.TFRecordWriter('/n/home05/tzhu/work/icgen2/data_graph/SAM_E6nnmetrics_1050_1450') as writer:
-    for x in range(10,15):
-        simulationpath = '/n/home05/tzhu/work/icgen2/gputest2/data/EMinus_Hadrons_seed_'+str(x)+'50_meta_data.parquet'
+#with tf.io.TFRecordWriter('/n/holyscratch01/arguelles_delgado_lab/Everyone/tzhu/hex_SAM_4nnIDmetrics_1_70') as writer:
+with tf.io.TFRecordWriter('/n/holyscratch01/arguelles_delgado_lab/Everyone/tzhu/ortho2SAM_4nnIDmetrics_1_70') as writer:
+    for x in range(1,71):
+        #simulationpath = '/n/holyscratch01/arguelles_delgado_lab/Everyone/tzhu/simu/hexa/data/MuMinus_Hadrons_seed_'+str(x)+'_meta_data.parquet'
+        simulationpath = '/n/home05/tzhu/work/icgen2/gputest2/data/MuMinus_Hadrons_seed_'+str(x)+'_meta_data.parquet'
+
         simu = rp(simulationpath)
         print(f'#######begin file {x}#######')
         for i in range(1000):
@@ -106,6 +123,7 @@ with tf.io.TFRecordWriter('/n/home05/tzhu/work/icgen2/data_graph/SAM_E6nnmetrics
                 counter+=1
                 example = tfgnn.write_example(graph)
                 writer.write(example.SerializeToString())
+        print(f'Now we have {counter} events in file')
 
 #################################
 print(counter)
